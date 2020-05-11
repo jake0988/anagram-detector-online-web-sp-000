d4f784d09ea1dbda1c683d1f2d75a7d9c4fb4c79
Jake G <jake098877@gmail.com>
2020-05-11 22:00:08 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,25 @@
-# Your code goes here!
+require 'pry'
+class Anagram
+attr_accessor :word
+
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(list)
+    array = []
+    @word = @word.split("")
+    @word.sort!
+    @word = @word.join
+    list.each do |i|
+    array = i.split("")
+    array.sort!
+    array = array.join
+    if @word = array
+      @word
+  end
+    end
+  end
+
+end
